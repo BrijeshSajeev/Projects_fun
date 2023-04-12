f0b440b88247f4a1fe6b538ec3cd042babcb6ffd
BrijeshSajeev <brijesh00300@gmail.com>
2023-04-12 17:31:41 +0530

    finished todo list project

--- todo_app/script.js
+++ todo_app/script.js
@@ -3,37 +3,67 @@ const input = document.querySelector('.input');
 const btnAdd = document.querySelector('.btn-add');
 // const btnDelete = document.querySelector('.btn-delete');
 const containerTask = document.querySelector('.tasks');
-let task={
-    tasks:[],
+class Task{
+
+    constructor(){
+        this.tasks=[];
+        this.getLocalStr();
+    }
+    render(){
+        containerTask.innerHTML='';
+        const HTML = this.tasks.map(text => `
+        <li>
+              <input type="checkbox">
+                <span>${text}</span>
+              <button class="noselect btn-delete" data-value="${text}"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
+        </li>
+        `).join('');
+        containerTask.insertAdjacentHTML('beforeend',HTML);
+        this.setLocalStr();
+    }
+    deleteTask(index){
+        this.tasks.splice(index,1);
+        this.render();
+    }
+    addElemnt(value){   
+        this.tasks.push(value);
+    }
+
+    addTask = function(){
+        if(input.value ==="") return;
+        this.addElemnt(input.value);
+        this.render();
+    }
+
+    setLocalStr(){
+        localStorage.setItem('task',JSON.stringify(this.tasks))
+    }
+    getLocalStr(){
+        const data = JSON.parse(localStorage.getItem('task'));
+
+        if (!data) return;
+
+        this.tasks = data;
+
+        this.render();
+        
+    }
 };
+ 
+const task = new Task();
 
-const addHTML = function(text){
-   
-    const HTML =`
-    <li>
-          <input type="checkbox">
-            <span>${text}</span>
-          <button class="noselect btn-delete" data-value="${text}"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-    </li>
-    `
-    containerTask.insertAdjacentHTML('beforeend',HTML)
-}
 
-const addTask = function(){
-    if(input.value ==="") return;
-    task.tasks.push(input.value);
-    addHTML(input.value);
-}
+
 
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    addTask();
+    task.addTask();
     form.reset();
 })
 
 btnAdd.addEventListener('click',function(e){
-    addTask();
+    task.addTask();
     form.reset();
 })
 
@@ -41,7 +71,10 @@ containerTask.addEventListener('click',function(e){
 
     const btn = e.target.closest('.btn-delete');
     if(!btn) return;
-    
-    console.log("delete btn",btn);
+
+    const x = task.tasks.indexOf(btn.dataset.value);
+    task.deleteTask(x);
 
 })
+
+
